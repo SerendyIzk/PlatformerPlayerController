efa416d97c5384a2ec6095e7ddc294eadeec90f8
MastWex <09dracon@gmail.com>
2024-06-27 21:53:36 +0300

    CameraUpdate
    -Загрузил Cinemachine(грубо говоря, это хорошо настроенная камера)
    -Сделал префаб камеры(пока неочень работает)

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CamController : MonoBehaviour
+{
+    public Camera Camera;
+    public CinemachineVirtualCamera CM;
+}
--- Assets/Scripts/Initialization.cs
+++ Assets/Scripts/Initialization.cs
@@ -2,16 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Cinemachine;
 
 public class Initialization : MonoBehaviour
 { /*
     [NonSerialized] public GameObject Player;
     [NonSerialized] public Rigidbody PlayerRb;
     [NonSerialized] public GroundedController PlayerGroundedController;
+    */
+    [NonSerialized] private Camera _camera;
+    [NonSerialized] private CinemachineVirtualCamera _CM;
+    [NonSerialized] private CamController _camController;
+    
 
-    private void Init() { Player = FindObjectOfType<VerticalPlayerMovement>().gameObject;
-                          PlayerRb = Player.GetComponent<Rigidbody>();
-                          PlayerGroundedController = Player.transform.GetChild(0).GetComponent<GroundedController>(); }
+    private void Init()
+    {
+        /*Player = FindObjectOfType<VerticalPlayerMovement>().gameObject;
+        PlayerRb = Player.GetComponent<Rigidbody>();
+        PlayerGroundedController = Player.transform.GetChild(0).GetComponent<GroundedController>();*/
+        _camera = FindObjectOfType<Camera>();
+        _CM = FindObjectOfType<CinemachineVirtualCamera>();
+        _camController = FindObjectOfType<CamController>();
+    }
+    private void InitOther()
+    {
+        _camController.Camera = _camera;
+        _camController.CM = _CM;
+    }
 
-    private void Awake() { Init(); } */
-}
+    private void Awake() { Init(); InitOther(); } 
+    }
