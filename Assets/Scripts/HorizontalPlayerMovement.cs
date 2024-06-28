73d65135bab2233308a06e33ec692cae9ab64749
SerendyIzk <dl0310@bk.ru>
2024-06-28 03:20:38 +0300

    Minor Update
    - Настроен параметр Coyote Time
    - Теперь можно посмотреть из других скриптов, куда движется игрок

--- Assets/Scripts/HorizontalPlayerMovement.cs
+++ Assets/Scripts/HorizontalPlayerMovement.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class HorizontalPlayerMovement : MonoBehaviour
 {
+    public Vector3 MoveVector { get; private set; }
     [SerializeField] private float _acceleration;
     [SerializeField] private float _deceleration;
     [SerializeField] private float _maxSpeed;
     private float _currentSpeed;
-    private Vector3 _moveVector;
     private bool _isShouldMove;
 
     private void DirectionDefiningUpdate() { _isShouldMove = false;
-                                             if (Input.GetKey(KeyCode.D)) { _moveVector = transform.right; _isShouldMove = true; }
-                                             if (Input.GetKey(KeyCode.A)) { _moveVector = -transform.right; _isShouldMove = true; } }
+                                             if (Input.GetKey(KeyCode.D)) { MoveVector = transform.right; _isShouldMove = true; }
+                                             if (Input.GetKey(KeyCode.A)) { MoveVector = -transform.right; _isShouldMove = true; } }
 
     private void SpeedUpdate() { _currentSpeed = _isShouldMove ? Mathf.Clamp(_currentSpeed + _acceleration, 0, _maxSpeed) : Mathf.Clamp(_currentSpeed - _deceleration, 0, _maxSpeed); }
 
-    private void MovementFixedUpdate() { transform.position += _moveVector * _currentSpeed * Time.fixedDeltaTime; }
+    private void MovementFixedUpdate() { transform.position += MoveVector * _currentSpeed * Time.fixedDeltaTime; }
 
     private void Update() { DirectionDefiningUpdate(); SpeedUpdate(); }
 
